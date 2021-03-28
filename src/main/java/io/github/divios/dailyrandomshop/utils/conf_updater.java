5a5cf45a9539671c566988a9b5e99c229d324a3c
Divios <serberpar@gmail.com>
2021-03-28 17:57:37 +0200

    updated

--- src/main/java/io/github/divios/dailyrandomshop/utils/conf_updater.java
+++ src/main/java/io/github/divios/dailyrandomshop/utils/conf_updater.java
@@ -15,7 +15,7 @@ public class conf_updater {
             io.github.divios.dailyrandomshop.main.getInstance();
 
     public static void check() {
-        String version = "2.2.3";
+        String version = "3.0.1";
         if (main.getConfig().getString("version", version).equals(version))
             return;
         update();
