6770354b2dd1ef2101cd9569f367df24cb8790af
Divios <serberpar@gmail.com>
2021-06-03 09:50:04 +0200

    migrate to gradle

--- src/main/java/io/github/divios/dailyrandomshop/builders/itemBuildersHooks/oraxenItems.java
+++ src/main/java/io/github/divios/dailyrandomshop/builders/itemBuildersHooks/oraxenItems.java
@@ -6,43 +6,24 @@ import org.bukkit.inventory.ItemStack;
 
 public class oraxenItems implements itemsBuilder{
 
+
     @Override
     public boolean isItem(ItemStack item) {
-        try {
-            return !utils.isEmpty(getUuid(item));
-        } catch (NoClassDefFoundError | NoSuchMethodError
-                | ExceptionInInitializerError e) {
-            return false;
-        }
+        return false;
     }
 
     @Override
     public ItemStack getItem(ItemStack item) {
-        try {
-        String s = OraxenItems.getIdByItem(item);
-        return OraxenItems.getItemById(s).getReferenceClone();
-        } catch (NoClassDefFoundError | NoSuchMethodError
-                | ExceptionInInitializerError e) {
-            return null;
-        }
+        return null;
     }
 
     @Override
     public String getUuid(ItemStack item) {
-        try {
-            return OraxenItems.getIdByItem(item);
-        } catch (NoClassDefFoundError | NoSuchMethodError
-                | ExceptionInInitializerError e) {
-            return null;
-        }
+        return null;
     }
 
     @Override
     public boolean updateItem(ItemStack toUpdate) {
-        if (!isItem(toUpdate)) return false;
-        ItemStack auxitem = getItem(toUpdate);
-        //dailyItem.transferDailyMetadata(toUpdate, auxitem);
-        utils.translateAllItemData(auxitem, toUpdate);
-        return true;
+        return false;
     }
 }
