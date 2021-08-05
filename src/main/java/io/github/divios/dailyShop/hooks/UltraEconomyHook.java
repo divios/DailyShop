5070aff4ff986ac6e38c6dfb7e69898f3a6e0f37
Divios <serberpar@gmail.com>
2021-08-05 12:17:41 +0200

    ultraEconomy support

---
+++ src/main/java/io/github/divios/dailyShop/hooks/UltraEconomyHook.java
@@ -0,0 +1,19 @@
+package io.github.divios.dailyShop.hooks;
+
+import io.github.divios.dailyShop.DailyShop;
+import me.TechsCode.UltraEconomy.UltraEconomy;
+import me.TechsCode.UltraEconomy.UltraEconomyAPI;
+
+public class UltraEconomyHook {
+
+    private static final DailyShop plugin = DailyShop.getInstance();
+    private static UltraEconomyAPI api = null;
+
+    public static void hook() {
+        plugin.getLogger().info("Hooked to UltraEconomy");
+        api = UltraEconomy.getAPI();
+    }
+
+    public static UltraEconomyAPI getApi() { return api; }
+
+}
