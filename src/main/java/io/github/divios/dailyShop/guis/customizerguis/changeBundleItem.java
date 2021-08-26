283bad3e889e7e562b7cd44558fc26c0023ee43d
Divios <serberpar@gmail.com>
2021-08-26 12:29:03 +0200

    optimize imports

--- src/main/java/io/github/divios/dailyShop/guis/customizerguis/changeBundleItem.java
+++ src/main/java/io/github/divios/dailyShop/guis/customizerguis/changeBundleItem.java
@@ -14,7 +14,6 @@ import org.bukkit.inventory.ItemFlag;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
