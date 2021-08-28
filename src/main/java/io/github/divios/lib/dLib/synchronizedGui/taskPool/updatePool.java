f5a1983af5027cd08d0ee90d661b967185aedf9d
Divios <serberpar@gmail.com>
2021-08-28 10:41:18 +0200

    pool is managed synchronously on syncMenu and is done async

--- src/main/java/io/github/divios/lib/dLib/synchronizedGui/taskPool/updatePool.java
+++ src/main/java/io/github/divios/lib/dLib/synchronizedGui/taskPool/updatePool.java
@@ -3,7 +3,10 @@ package io.github.divios.lib.dLib.synchronizedGui.taskPool;
 import io.github.divios.core_lib.Schedulers;
 import io.github.divios.core_lib.itemutils.ItemBuilder;
 import io.github.divios.core_lib.itemutils.ItemUtils;
+import io.github.divios.dailyShop.lorestategy.loreStrategy;
+import io.github.divios.dailyShop.lorestategy.shopItemsLore;
 import io.github.divios.lib.dLib.dInventory;
+import io.github.divios.lib.dLib.dItem;
 import io.github.divios.lib.dLib.synchronizedGui.singleGui.singleGui;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.inventory.Inventory;
@@ -11,6 +14,7 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.IntStream;
 
@@ -19,30 +23,9 @@ public class updatePool {
     private static final Set<singleGui> bucket = new HashSet<>();
 
     static {
-        Schedulers.sync().runRepeating(() -> {
-            bucket.stream().parallel().forEach(gui -> {
-                dInventory base = gui.getInventory();
-                IntStream.range(0, base.getSize())
-                        .filter(value -> !base.getOpenSlots().contains(value))
-                        .filter(value -> !ItemUtils.isEmpty(base.getInventory().getItem(value)))
-                        .forEach(value -> {
-
-                            Inventory inv = base.getInventory();
-                            ItemStack oldItem = gui.getBase().getInventory().getItem(value);
-                            ItemBuilder newItem = ItemBuilder.of(oldItem.clone()).setLore(Collections.emptyList());
-
-                            newItem = newItem.setName(PlaceholderAPI.setPlaceholders(gui.getPlayer(), ItemUtils.getName(oldItem)));
-
-                            for (String s : ItemUtils.getLore(oldItem))
-                                newItem = newItem.addLore(PlaceholderAPI.setPlaceholders(gui.getPlayer(), s));
-
-                            inv.setItem(value, newItem);
-
-                        });
-            });
-        }, 20L, 20L);
+        Schedulers.async().runRepeating(() -> bucket.stream().parallel().forEach(singleGui::updateTask),
+                10L, 10L);
     }
-
 
     public static void subscribe(singleGui gui) {
         bucket.add(gui);
@@ -51,5 +34,6 @@ public class updatePool {
     public static void cancel(singleGui gui) {
         bucket.remove(gui);
     }
+
 
 }
