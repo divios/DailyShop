bd9d1c348edf2e61d935a37a23b8c074dcf4e78d
Divios <serberpar@gmail.com>
2021-09-05 20:46:39 +0200

    implemented stock amount

--- src/main/java/io/github/divios/dailyShop/events/updateItemEvent.java
+++ src/main/java/io/github/divios/dailyShop/events/updateItemEvent.java
@@ -14,27 +14,40 @@ public class updateItemEvent extends Event {
     private static final HandlerList HANDLERS_LIST = new HandlerList();
 
     private final dItem item;
-
+    private final int amount;
     private final Player p;
-    private final updatetype type;
+    private updatetype type;
     private final dShop shop;
     private final Timestamp timestamp;
 
     public updateItemEvent(ItemStack item, updatetype type, dShop shop) {
-        this(null, new dItem(item), type, shop);
+        this(null, new dItem(item), 1, type, shop);
     }
 
     public updateItemEvent(dItem item, updatetype type, dShop shop) {
-        this(null, item, type, shop);
+        this(null, item, 1, type, shop);
     }
 
     public updateItemEvent(Player p, ItemStack item, updatetype type, dShop shop) {
-        this(p, new dItem(item), type, shop);
+        this(p, new dItem(item), 1, type, shop);
     }
 
-    public updateItemEvent(Player p, dItem item, updatetype type, dShop shop) {
+    public updateItemEvent(ItemStack item, int amount, updatetype type, dShop shop) {
+        this(null, new dItem(item), amount, type, shop);
+    }
+
+    public updateItemEvent(dItem item, int amount, updatetype type, dShop shop) {
+        this(null, item, amount, type, shop);
+    }
+
+    public updateItemEvent(Player p, ItemStack item, int amount, updatetype type, dShop shop) {
+        this(p, new dItem(item), amount, type, shop);
+    }
+
+    public updateItemEvent(Player p, dItem item, int amount, updatetype type, dShop shop) {
         this.p = p;
-        this.item = item;
+        this.item = item.clone();
+        this.amount = amount;
         this.type = type;
         this.shop = shop;
         this.timestamp = new Timestamp(System.currentTimeMillis());
@@ -46,6 +59,10 @@ public class updateItemEvent extends Event {
 
     public dItem getItem() { return this.item; }
 
+    public int getAmount() {
+        return amount;
+    }
+
     public updatetype getType() {
         return type;
     }
@@ -53,6 +70,10 @@ public class updateItemEvent extends Event {
     public dShop getShop() { return this.shop; }
 
     public Timestamp getTimestamp() { return this.timestamp; }
+
+    public void setType(updatetype type) {
+        this.type = type;
+    }
 
     @NotNull
     @Override
