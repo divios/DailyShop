1f077ace982c589a7ce9cfc6577c48e865684ae8
Divios <serberpar@gmail.com>
2021-09-05 20:44:34 +0200

    concurrent set

--- src/main/java/io/github/divios/lib/dLib/synchronizedGui/taskPool/updatePool.java
+++ src/main/java/io/github/divios/lib/dLib/synchronizedGui/taskPool/updatePool.java
@@ -1,26 +1,18 @@
 package io.github.divios.lib.dLib.synchronizedGui.taskPool;
 
+import com.google.common.collect.Sets;
 import io.github.divios.core_lib.Schedulers;
-import io.github.divios.core_lib.itemutils.ItemBuilder;
-import io.github.divios.core_lib.itemutils.ItemUtils;
-import io.github.divios.dailyShop.lorestategy.loreStrategy;
-import io.github.divios.dailyShop.lorestategy.shopItemsLore;
-import io.github.divios.lib.dLib.dInventory;
-import io.github.divios.lib.dLib.dItem;
 import io.github.divios.lib.dLib.synchronizedGui.singleGui.singleGui;
-import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.IntStream;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class updatePool {
 
-    private static final Set<singleGui> bucket = Collections.synchronizedSet(new HashSet<>());
+    private static final Set<singleGui> bucket = Sets.newConcurrentHashSet();
 
     static {
         Schedulers.async().runRepeating(() -> bucket.stream().parallel().forEach(singleGui::updateTask),
