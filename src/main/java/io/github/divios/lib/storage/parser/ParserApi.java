cf74f056a86fda6aefdee7a79f412e4485530297
Divios <serberpar@gmail.com>
2021-09-13 13:33:25 +0200

    fixed integers as doubles on yaml

--- src/main/java/io/github/divios/lib/storage/parser/ParserApi.java
+++ src/main/java/io/github/divios/lib/storage/parser/ParserApi.java
@@ -1,7 +1,8 @@
 package io.github.divios.lib.storage.parser;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.*;
+import com.google.gson.internal.LinkedTreeMap;
 import io.github.divios.core_lib.utils.Log;
 import io.github.divios.dailyShop.DailyShop;
 import io.github.divios.dailyShop.utils.FileUtils;
@@ -13,6 +14,7 @@ import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -26,13 +28,17 @@ public class ParserApi {
 
         shop.getItems().forEach(dItem -> itemsCollect.put(dItem.getUid(), dItemState.of(dItem.getItem())));
 
-        String json = new GsonBuilder().setPrettyPrinting().create().toJson(new jsonEntry(shop.getName(), itemsCollect, shop));
+        String json = new GsonBuilder()
+                .create().toJson(new jsonEntry(shop.getName(), itemsCollect, shop));
+
         File data = new File(DailyShop.getInstance().getDataFolder() + File.separator + "parser", shop.getName() + ".yml");
         if (!data.exists()) {
             FileUtils.createFile(data);
         }
 
-        Map map = new Gson().fromJson(json, Map.class);
+        Map map = new GsonBuilder().registerTypeAdapter(new TypeToken<Map <String, Object>>(){}.getType(),  new MapDeserializerDoubleAsIntFix())
+                .create().fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
+
         DumperOptions options = new DumperOptions();
         options.setIndent(2);
         options.setAllowUnicode(true);
@@ -138,6 +144,53 @@ public class ParserApi {
             return items;
         }
 
+    }
+
+    public static class MapDeserializerDoubleAsIntFix implements JsonDeserializer<Map<String, Object>>{
+
+        @Override  @SuppressWarnings("unchecked")
+        public Map<String, Object> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            return (Map<String, Object>) read(json);
+        }
+
+        public Object read(JsonElement in) {
+
+            if(in.isJsonArray()){
+                List<Object> list = new ArrayList<Object>();
+                JsonArray arr = in.getAsJsonArray();
+                for (JsonElement anArr : arr) {
+                    list.add(read(anArr));
+                }
+                return list;
+            }else if(in.isJsonObject()){
+                Map<String, Object> map = new LinkedTreeMap<>();
+                JsonObject obj = in.getAsJsonObject();
+                Set<Map.Entry<String, JsonElement>> entitySet = obj.entrySet();
+                for(Map.Entry<String, JsonElement> entry: entitySet){
+                    map.put(entry.getKey(), read(entry.getValue()));
+                }
+                return map;
+            }else if( in.isJsonPrimitive()){
+                JsonPrimitive prim = in.getAsJsonPrimitive();
+                if(prim.isBoolean()){
+                    return prim.getAsBoolean();
+                }else if(prim.isString()){
+                    return prim.getAsString();
+                }else if(prim.isNumber()){
+
+                    Number num = prim.getAsNumber();
+                    // here you can handle double int/long values
+                    // and return any type you want
+                    // this solution will transform 3.0 float to long values
+                    if(Math.ceil(num.doubleValue())  == num.longValue())
+                        return num.longValue();
+                    else{
+                        return num.doubleValue();
+                    }
+                }
+            }
+            return null;
+        }
     }
 
 
