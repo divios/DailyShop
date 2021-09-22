a67423d619184cb9f0a92e761705d4c2e3153a4c
Divios <serberpar@gmail.com>
2021-09-22 08:49:37 +0200

    refactored cleanCode

--- src/main/java/io/github/divios/dailyShop/lorestategy/loreStrategy.java
+++ src/main/java/io/github/divios/dailyShop/lorestategy/loreStrategy.java
@@ -4,14 +4,7 @@ import org.bukkit.inventory.ItemStack;
 
 public interface loreStrategy {
 
-    void setLore(ItemStack item);
-
     default ItemStack applyLore(ItemStack item) { return applyLore(item, null); }
-
     ItemStack applyLore(ItemStack item, Object... data);
-
-    void removeLore(ItemStack item);
-
-    void update(ItemStack item);
 
 }
