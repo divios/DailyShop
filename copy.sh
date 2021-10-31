2c729f5d377e5f3143a29fba5c7858f172c87efa
divios <serberpar@gmail.com>
2021-10-31 15:38:47 +0100

    updated

--- copy.sh
+++ copy.sh
@@ -1,1 +1,1 @@
-cp ~/IdeaProjects/DailyShop/build/libs/DailyShop-3.5.4-all.jar ~/Escritorio/server_pruebas/1.16/plugins/DailyShop-3.5.4.jar
+cp ~/IdeaProjects/DailyShop/build/libs/DailyShop-3.5.5-all.jar ~/Escritorio/server_pruebas/1.16/plugins/DailyShop-3.5.5.jar
