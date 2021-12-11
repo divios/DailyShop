f1ba2ccef9cabc8f5cabf2f13fbac4102effc602
divios <serberpar@gmail.com>
2021-12-11 21:58:43 +0100

    Added async methods

--- src/main/java/io/github/divios/lib/serialize/serializerApi.java
+++ src/main/java/io/github/divios/lib/serialize/serializerApi.java
@@ -2,6 +2,7 @@ package io.github.divios.lib.serialize;
 
 import com.google.common.base.Preconditions;
 import io.github.divios.core_lib.cache.Lazy;
+import io.github.divios.core_lib.scheduler.Schedulers;
 import io.github.divios.core_lib.utils.Log;
 import io.github.divios.dailyShop.DailyShop;
 import io.github.divios.dailyShop.utils.FileUtils;
@@ -10,6 +11,7 @@ import io.github.divios.lib.dLib.dShop;
 
 import java.io.File;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 public class serializerApi {
 
@@ -31,6 +33,14 @@ public class serializerApi {
         Objects.requireNonNull(data, "data cannot be null");
         Preconditions.checkArgument(data.exists(), "The file does not exist");
         return dShop.encodeOptions.JSON.fromJson(Utils.getJsonFromFile(data));
+    }
+
+    public static void saveShopToFileAsync(dShop shop) {
+        Schedulers.async().run(() -> saveShopToFile(shop));
+    }
+
+    public static CompletableFuture<dShop> getShopFromFileAsync(File data) {
+        return CompletableFuture.supplyAsync(() -> getShopFromFile(data));
     }
 
 }
